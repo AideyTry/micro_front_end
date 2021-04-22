0cc76bc8f463aa943add7f6bea7f18edd5a56478
aidenfly <jason_dlb@sina.cn>
2021-04-22 17:54:50 +0800

    add angular app

---
+++ angular-app/src/public-path.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Aiden
+ * @Date: 2021-04-22 17:47:37
+ * @LastEditTime: 2021-04-22 17:47:38
+ * @LastEditors: Aiden
+ * @Description: 
+ * @Email: aiden.dai@bayconnect.com.cn
+ */
+if (window.__POWERED_BY_QIANKUN__) {
+    // eslint-disable-next-line no-undef
+    __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+  }
